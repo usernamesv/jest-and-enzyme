4237fe463a1f44003f496c5528f27a244c2ef7c3
Abhishek Hunny <Abhishek@Hunny>
2022-01-01 02:34:08 +0530

    adding toggle.test.js and ppt

---
+++ src/ToggleComponent.test.js
@@ -0,0 +1,58 @@
+// describe('ToggleComponent', () => {
+//     it('Should be true', () => {
+//        const test = true;
+//        expect(test).toBe(true);
+//     });
+
+//     it('Should be false', () => {
+//         const test = 'Shubham';
+//         expect(test).toBe('Ankit');
+//      });
+     
+//  });
+
+
+
+
+
+
+
+
+
+
+// ----Common MATCHERS-----
+
+// test ('2 +2 =4' , ()=>{
+//     expect(2+2).toBe(4);
+// })
+
+// --- Number Matcher
+
+// test ('2 +2 =4' , ()=>{
+//     const value = 2+2
+//     expect(value).toBeGreaterThan(4);
+// })
+
+// --- Floating Matcher
+
+// test('adding floating point numbers', () => {
+//     const value = 0.1 + 0.2;
+//     expect(value).toBe(0.3);           //This won't work because of rounding error
+//     expect(value).toBeCloseTo(0.3); // This works.
+//   });
+
+// --- String Matcher
+
+// test ('There is no "P" in Abhishek', ()=>{
+//     expect('Abhishek').not.toMatch(/P/);
+//     })
+
+// --- Array Matcher  
+
+// var employee =[];
+// employee =['Alex','David','John','Abhishek'];
+// test("Except 'Abhishek' in employee Array", ()=>{
+//     expect(employee).toContain('Abhishek')
+// })
+
+
